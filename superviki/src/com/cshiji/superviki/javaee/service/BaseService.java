dd79679b437b872c06860f8abb8c8962ae000a7b
orange <cj@MS-20160324YHFU>
2017-02-22 16:10:36 +0800

    aaa

---
+++ superviki/src/com/cshiji/superviki/javaee/service/BaseService.java
@@ -0,0 +1,16 @@
+
+package com.cshiji.superviki.javaee.service;
+
+import com.cshiji.superviki.base.dao.impl.HibernateDao;
+
+/**
+ * serviceImpl继承baseService可以使用hibernate
+ * TODO javadoc for com.cshiji.superviki.javaee.service.BaseService
+ * @Copyright:
+ * @author: cj
+ * @since: 2017年2月22日
+ */
+public class BaseService extends HibernateDao {
+
+}
+
